ad36f0767f42bb375ce65b77bf43c2275ec8f452
ilkay bülbül <75014140+ilkaybulbul@users.noreply.github.com>
2023-08-20 23:17:51 +0300

    Add files via upload

--- day4/main.js
+++ day4/main.js
@@ -1,4 +1,18 @@
-function copy (id) {
-    document.getElementById(id).select()
-    document.execCommand('copy')
-} 
+const copy = document.querySelector('.button')
+const VALUE = document.querySelector('#copy-url')
+const ALERT = document.querySelector('.alert')
+
+copy.addEventListener('click', function() {
+
+    if(VALUE.value === '') {
+        ALERT.classList.add('error')
+    }
+    navigator.clipboard.writeText(VALUE.value)
+    
+    ALERT.classList.add('active')
+
+    setTimeout(function() {
+        ALERT.classList.remove('active')
+    },2000)
+})
+
